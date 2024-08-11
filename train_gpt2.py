453d394c05d4ad61ea3e7d0222a2a836e1518aab
YantoPersonal <ieuang@hotmail.com>
2024-08-11 21:40:51 +0100

    Updated train gpt2, including the ability to load in the state dict of hugging face models to our gpt2 codebase.

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,12 +1,73 @@
 """
 GPT2: - Reproducing the 124M parameter model.
+
+
+Additional Information:
+- "contiguous" function - https://stackoverflow.com/questions/48915810/what-does-contiguous-do-in-pytorch
+
+- **config_args allows for flexible parameter passing when creating GPTConfig instances or initializing models.
+It's particularly useful when you want to allow users or other parts of your code to specify only the parameters
+they want to change, while using default values for the rest.
 """
 
 # Import Statements:
+import math
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # 'bias' is what OpenAI call the mask in the GPT2-124M model
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()  # Batch Size, Token Sequence, Token Embedding (n_embd)
+        # Calculate the query, key, values for all heads in a batch and move head forward to be the batch.
+        # nh is the "number of heads", hs is the "head size" and C (number of channels) = nh * hs
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        # Query, Keys and Values for all the heads within the multi-head attention in one array.
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        # Attention (materializes the (T, T) matrix for all the queries * keys.
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))  # (B, nh, T, hs) * (B, T, nh, hs)) * Normalize
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v  # (B, nh, T, T) x (b, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # Concatenation Operation
+        # Output projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')  # Approximate is now dated, but was used for GPT2.
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 
 class Block(nn.Module):
@@ -26,18 +87,18 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024  # max sequence length (context window)
+    vocab_size: int = 50257  # number of tokens: 50,000 BPE + 256 utf-8 + 1 <|endoftext|>
+    n_layer: int = 12  # number of  layers
+    n_head: int = 12  # number of heads
+    n_embd: int = 768  # embedding dimension
 
 
 class GPT(nn.Module):
 
     def __init__(self, config):
+        super().__init__()
         self.config = config
-
         self.transformer = nn.ModuleDict(dict(
             wte=nn.Embedding(config.vocab_size, config.n_embd),  # Token Embeddings
             wpe=nn.Embedding(config.block_size, config.n_embd),  # Positional Embeddings
@@ -45,4 +106,54 @@ class GPT(nn.Module):
             ln_f=nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    # What are class methods? https://claude.ai/chat/3129336f-ee60-4310-80e2-b9efb6bdcf17
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """
+        Loads pre-trained GPT-2 model weights from huggingface
+        """
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print(f"loading weights from pretrained gpt: {model_type}")
+
+        # n_layer, n_head, and n_embd are determined from model_type
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024),  # 250M params
+            'gpt2-large': dict(n_layer=24, n_head=20, n_embd=1280),  # 774M params
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),  # 1550M params
+        }[model_type]
+        config_args['vocab_size'] = 50257  # Identical for all GPT2 models
+        config_args['block_size'] = 1024  # Identical for all GPT2 models
+
+        # Create our from-scratch initialized GPT model:
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]  # discard the mask in loading weights
+
+        # Initialize huggingface/transformer model:
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # Copy over parameters while ensuring names and shapes match:
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]  # Ignore mask in HuggingFace
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]  # Ignore mask in HuggingFace
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight'] # Manual Fix
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # Special treatment for manual fix, where layers are not in PyTorch format
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
 
